ee059457d3aaaa587a5e3b2de78b31596e2cff7a
Mike Smith <grimbough@gmail.com>
2023-03-27 14:21:26 +0200

    Build xtensor-zarr from source

--- implementations/xtensor_zarr/driver.sh
+++ implementations/xtensor_zarr/driver.sh
@@ -10,14 +10,25 @@ write(){
     cd "${IMPL}"
 
     set +u # Due to GDAL_DATA
+    
+    if [ ! -d xtensor-zarr ]; then
+      git clone https://github.com/xtensor-stack/xtensor-zarr.git
+    fi
+    cd xtensor-zarr
+    mkdir -p build
+    cd build
+    export LDFLAGS="${LDFLAGS} -Wl,-rpath,$CONDA_PREFIX/lib -Wl,-rpath,$PWD"
+    cmake .. -DCMAKE_PREFIX_PATH=$CONDA_PREFIX -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCMAKE_INSTALL_LIBDIR=lib -DXTENSOR_ZARR_DISABLE_ARCH_NATIVE=ON
+    make install
+    cd ../../
 
     rm -rf build
     mkdir build
     cd build
     export LDFLAGS="${LDFLAGS} -Wl,-rpath,$CONDA_PREFIX/lib -Wl,-rpath,$PWD"
     export LINKFLAGS="${LDFLAGS}"
-    cmake .. -DCMAKE_PREFIX_PATH=$CONDA_PREFIX -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_TYPE=Debug
-    make VERBOSE=1 run
+    cmake .. -DCMAKE_PREFIX_PATH=$CONDA_PREFIX -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCMAKE_INSTALL_LIBDIR=lib 
+    make run
 }
 
 . $ROOT/.conda_driver.sh
